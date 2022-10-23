94e38c1d22c6832d1038345c11dee875a165d0a6
常兆海 <1341195069@qq.com>
2022-10-23 11:07:12 +0800

    集合类实战操作： 反转链表： 新增递归的实现方法： 关键点：每次传入的参数与返回的参数！

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+int i=1;
+int sum=5;
+        System.out.println(test(5,1));
+
     }
     /***
      * 可供的实现方法;
@@ -9,5 +13,23 @@ public class Main {
      * 3： 新建链表法;(迭代法)
      * 4：
      */
+
     //todo  1： 递归：
+
+    /***
+     * 每次递归传入的参数就在递归的过程中定死了；不会改变！！！
+     * 然后倒着进行每次计算
+     */
+    public  static  int test(int sum,int i){
+
+        if (i>5)return sum;
+
+        sum=sum*i;
+        i++;
+        test(sum, i);
+        System.out.println("sum=sum*i:"+"sum: "+sum+"i:"+i);
+        return sum;
+
+
+    }
 }
--- src/反转链表.java
+++ src/反转链表.java
@@ -12,14 +12,16 @@ public class 反转链表 {
          */
         sout(head);
         System.out.println();
-        //    sout(reverseDiGui(head));
-sout(reverseZhan(head));
+            sout(reverseDiGui(head));
+//sout(reverseZhan(head));
 
- //  sout(reverseZhan1(head));
+        //  sout(reverseZhan1(head));
 
 
-    //sout(head);
+        //sout(head);
 
+
+        //sout(test(head));
     }
 
     public static class Node {
@@ -44,8 +46,14 @@ sout(reverseZhan(head));
      * @return 反序列的链表
      * 思路： 倒着赋值;递归的特性
      */
-//    public static Node reverseDiGui(Node head) {
-//    }
+    public static Node reverseDiGui(Node head) {
+        if (head==null||head.next==null)return head;//递归的返回条件为空就返
+        Node rehead =reverseDiGui(head.next);//触底。直接寻找到7-》9
+        //对传入的参数做处理，将本次的参数倒置，并将参数剪短，控制每次处理参数为两个
+        head.next.next=head;
+        head.next=null;
+        return  rehead;
+    }
 
     /***
      * // 栈： 操作（integer） （不知道违不违反规则）
@@ -57,7 +65,7 @@ sout(reverseZhan(head));
 
     public static Node reverseZhan(Node head) {
         Node rehead = head; //做新节点遍历重新赋值操作
-        Node header=head;//💥记录头节点位置；
+        Node header = head;//💥记录头节点位置；
         Stack<Integer> stack = new Stack<>();
         int i = 0;
         while (head != null) {
@@ -66,13 +74,14 @@ sout(reverseZhan(head));
             i++;
         }//将链表元素全部写入
         while (i != 0) {
-          rehead.value=stack.pop();
-           rehead=rehead.next;
+            rehead.value = stack.pop();
+            rehead = rehead.next;
             i--;
         }
 
         return header;//返回的是末尾值
     }
+
     /***
      * // 栈： 操作（Node）
      * @param head 传入值
@@ -80,23 +89,27 @@ sout(reverseZhan(head));
      *  head 重新赋值的链表中 负责遍历操作时使用；直接在源链表上操作
      *  temp💥记录重新赋值的链表的头节点位置，不然返回值不能操作（输出本质还是一次遍历） 这不会导致无法输出
      */
-    public static Node reverseZhan1(Node head){
+    public static Node reverseZhan1(Node head) {
         //todo 栈中node模式的链表
-        Stack<Node> stack=new Stack<>();
+        Stack<Node> stack = new Stack<>();
         //压栈     1.把链表中的节点入栈
-        while (head!=null){
+        while (head != null) {
             stack.push(head);
-            head=head.next;
+            head = head.next;
         }
-        if (!stack.isEmpty()) head=stack.pop();else return null;//创建头节点记录位置
-       Node temp=head;  //💥head记录头指针位置；temp做遍历
-        while (!stack.isEmpty()){
-            temp.next=stack.pop();//pop出来的是一串 所以可以直接在next节点上重新覆盖，以达到重写目的
-            temp= temp.next; //在下一个节点做操作等同于截断
-        }temp.next=null;//将最后一个截断只要他的一个节点；不然就循环了
+        if (!stack.isEmpty()) head = stack.pop();
+        else return null;//创建头节点记录位置
+        Node temp = head;  //💥head记录头指针位置；temp做遍历
+        while (!stack.isEmpty()) {
+            temp.next = stack.pop();//pop出来的是一串 所以可以直接在next节点上重新覆盖，以达到重写目的
+            temp = temp.next; //在下一个节点做操作等同于截断
+        }
+        temp.next = null;//将最后一个截断只要他的一个节点；不然就循环了
         return head;
 
     }
+
+
 
 
     //todo 新建链表法;遍历单链表中的每一个节点，一个一个进行链接，如下图所示：
